b6b317797fb1a480b92dd92039231a8bdacf1407
Akash <abirakash1987@gmail.com>
2025-12-28 17:12:11 +0600

    fixed typo

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URL, {
+        const conn = await mongoose.connect(process.env.DATABASE_URL);
         console.log(`✓ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`❌ MongoDB Connection Error: ${error.message}`);
